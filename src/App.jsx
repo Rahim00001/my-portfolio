f36b1ab2c307535dcdadff6baaf945c5e501e83c
Rahim00001 <rahimlpt2022@gmail.com>
2023-12-13 01:14:05 +0600

    navbar and footer added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-center text-6xl'>Portfolio</h2>
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
